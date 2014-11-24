4fda006079a3c60deb0a8fd4606f355aa0c00d2b
David <kioltk@gmail.com>
2014-11-24 23:34:47 +0200

    Profile and Friends

--- app/src/main/java/org/happysanta/messenger/about/AboutFragment.java
+++ app/src/main/java/org/happysanta/messenger/about/AboutFragment.java
@@ -1,9 +1,25 @@
 package org.happysanta.messenger.about;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.happysanta.messenger.R;
 
 /**
  * Created by Jesus Christ. Amen.
  */
 public class AboutFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+
+
+
+        return rootView;
+    }
 }
