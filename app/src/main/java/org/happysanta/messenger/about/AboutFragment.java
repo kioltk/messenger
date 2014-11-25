530de3e91494e55907475babf3718f7b66576064
David <kioltk@gmail.com>
2014-11-25 23:36:32 +0200

    About buttons

--- app/src/main/java/org/happysanta/messenger/about/AboutFragment.java
+++ app/src/main/java/org/happysanta/messenger/about/AboutFragment.java
@@ -1,11 +1,14 @@
 package org.happysanta.messenger.about;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import org.happysanta.messenger.R;
 
@@ -18,7 +21,46 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
+        Button helpButton = (Button) rootView.findViewById(R.id.help);
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.vk.com/help"));
+                startActivity(browserIntent);
+            }
+        });
+
+        Button privacyButton = (Button) rootView.findViewById(R.id.privacy);
+        privacyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.vk.com/privacy"));
+                startActivity(browserIntent);
+            }
+        });
+
+        Button termsButton = (Button) rootView.findViewById(R.id.terms);
+        termsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.vk.com/terms"));
+                startActivity(browserIntent);
+            }
+        });
+
+        Button reportButton = (Button) rootView.findViewById(R.id.report);
+        reportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.vk.com/report"));
+                startActivity(browserIntent);
+            }
+        });
 
         return rootView;
     }
