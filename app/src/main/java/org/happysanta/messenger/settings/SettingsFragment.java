ee1211bd1fde81953ff3b74990dcfaf34a011653
David <kioltk@gmail.com>
2014-12-21 00:20:55 +0200

    Library added

--- app/src/main/java/org/happysanta/messenger/settings/SettingsFragment.java
+++ app/src/main/java/org/happysanta/messenger/settings/SettingsFragment.java
@@ -2,9 +2,12 @@ package org.happysanta.messenger.settings;
 
 import android.support.v4.app.Fragment;
 
+import com.github.machinarius.preferencefragment.PreferenceFragment;
+
 /**
  * Created by Jesus Christ. Amen.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragment {
+
 
 }
