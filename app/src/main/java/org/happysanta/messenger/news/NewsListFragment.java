a2ec65eaa649e12b317cf2b5434964ed0279d013
insidefun <mistique00@mail.ru>
2015-01-01 18:36:39 +0200

    cardView

--- app/src/main/java/org/happysanta/messenger/news/NewsListFragment.java
+++ app/src/main/java/org/happysanta/messenger/news/NewsListFragment.java
@@ -1,9 +1,21 @@
 package org.happysanta.messenger.news;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import org.happysanta.messenger.R;
 
 /**
  * Created by Jesus Christ. Amen.
  */
 public class NewsListFragment extends Fragment {
+    private View cardView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        cardView = inflater.inflate(R.layout.item_news, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
