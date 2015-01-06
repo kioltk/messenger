e37ec706bf35dae4713406564cd83e2f13fa142b
insidefun <mistique00@mail.ru>
2015-01-06 12:57:35 +0200

    create NewsAdapter create news list

---
+++ app/src/main/java/org/happysanta/messenger/news/NewsAdapter.java
@@ -0,0 +1,67 @@
+package org.happysanta.messenger.news;
+
+import android.app.Activity;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import com.vk.sdk.api.model.VKApiMessage;
+import com.vk.sdk.api.model.VKApiPost;
+import com.vk.sdk.api.model.VKList;
+
+import org.happysanta.messenger.R;
+
+/**
+ * Created by insidefun on 01.01.2015.
+ */
+public class NewsAdapter extends BaseAdapter {
+    private final Activity activity;
+    private VKList<VKApiPost> newsList;
+
+    public NewsAdapter(Activity activity, VKList<VKApiPost> newsList) {
+        this.activity = activity;
+        this.newsList = newsList;
+    }
+
+
+    @Override
+    public int getCount() {
+        return newsList.size()+5;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return newsList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View itemView = LayoutInflater.from(activity).inflate(R.layout.item_news, null);
+        CardView cardView = (CardView) itemView.findViewById(R.id.card_view);
+        ImageView photoView = (ImageView) itemView.findViewById(R.id.user_photo);
+        ImageView menuView = (ImageView) itemView.findViewById(R.id.news_menu);
+        TextView nameView = (TextView) itemView.findViewById(R.id.user_name);
+        TextView textView = (TextView) itemView.findViewById(R.id.news_body);
+        final TextView dateView = (TextView) itemView.findViewById(R.id.news_date);
+
+        final VKApiPost post = (VKApiPost) getItem(position);
+
+
+        dateView.setText("" + post.date);
+
+        return itemView;
+    }
+
+
+}
--- app/src/main/java/org/happysanta/messenger/news/NewsListFragment.java
+++ app/src/main/java/org/happysanta/messenger/news/NewsListFragment.java
@@ -3,19 +3,49 @@ package org.happysanta.messenger.news;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.model.VKApiPost;
+import com.vk.sdk.api.model.VKList;
+
 import org.happysanta.messenger.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jesus Christ. Amen.
  */
 public class NewsListFragment extends Fragment {
+
+    // core
+    private VKList<VKApiPost> newsList;
+
+    // ui
     private View cardView;
+    private ListView listView;
+    private NewsAdapter newsAdapter;
+
+    public NewsListFragment(){
+    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         cardView = inflater.inflate(R.layout.item_news, container, false);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        listView = (ListView) getActivity().findViewById(R.id.list_news);
+
+        newsAdapter = new NewsAdapter(getActivity(), newsList);
+        //listView.setAdapter(newsAdapter); | NullPointerException
+
+        return cardView;
     }
 }
