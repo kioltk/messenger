61f3a3ffd885c07e5b710be8e7b86e9151b6f271
David <kioltk@gmail.com>
2015-01-13 21:05:18 +0200

    Clearing

--- app/src/main/java/org/happysanta/messenger/main/NavigationFragment.java
+++ app/src/main/java/org/happysanta/messenger/main/NavigationFragment.java
@@ -294,18 +294,6 @@ public class NavigationFragment extends Fragment {
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
-
-    /**
-     * Per the navigation drawer design guidelines, updates the action bar to show the global app
-     * 'context', rather than just what's in the current screen.
-     */
-    private void showGlobalContextActionBar() {
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setTitle(R.string.app_name);
-    }
-
     private ActionBar getActionBar() {
         return ((ActionBarActivity) getActivity()).getSupportActionBar();
     }
