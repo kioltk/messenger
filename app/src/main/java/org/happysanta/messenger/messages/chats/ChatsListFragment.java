ac06ae4a37ed8a9993b29424c33a801c8ee688fa
David <kioltk@gmail.com>
2015-04-19 17:46:23 +0300

    Commits merging

--- app/src/main/java/org/happysanta/messenger/messages/chats/ChatsListFragment.java
+++ app/src/main/java/org/happysanta/messenger/messages/chats/ChatsListFragment.java
@@ -1,18 +1,13 @@
 package org.happysanta.messenger.messages.chats;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -117,7 +112,7 @@ public class ChatsListFragment extends BaseFragment {
                 @Override
                 public void onClick(View view) {
                     dialogs.get(position);
-                    startActivity(ChatActivity.getActivityIntent(getActivity(), dialog));
+                    startActivity(ChatActivity.openChat(getActivity(), dialog));
                 }
             });
             titleView.setText(dialog.getTitle());
